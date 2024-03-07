e622327fd33559f53c888279655721498a5c9bc0
Afpa <alex.nesvit2000@gmail.com>
2024-03-07 09:58:49 +0100

    php base V41:24

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 // php -S localhost:8000  pour lance le serveur
 // ctr+c  pour arreter le serveur
 
-
 $a = 100; // number- integer, int
 $b = 14.12; // float
 $c = 'Alex'; // string-str
@@ -16,3 +15,9 @@ $e = array(    // massiv-array
 
 // echo $a; //echo $b; //echo $c; //echo $d; // pour afficher
 echo $e[2]; //affiche Alex, 3 element 
+
+ $g = array(
+
+ );
+
+ echo $g[3];
