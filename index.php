b144c618457f120d1d00f16511cb82127a2238bb
Afpa <alex.nesvit2000@gmail.com>
2024-03-07 08:43:45 +0100

    php base V1.3

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
 <?php
 
+// php -S localhost:8000  pour lance le serveur
+// ctr+c  pour arreter le serveur
+
+
 $a = 100; // number- integer, int
 $b = 14.12; // float
 $c = 'Alex'; // string-str
 $d = "Alex"; // string-str
+$f = false; // boolean
 $e = array(1,2,3); // massiv-array
 
-// echo $a; //echo $b; //echo $c; //echo $d; // - vivesti znachenie
-echo $e[2]; //vivodit (3), eto 3 element 0, 1, (2)
+// echo $a; //echo $b; //echo $c; //echo $d; // pour afficher
+echo $e[2]; //affiche (3), 3 element 0, 1, (2)
